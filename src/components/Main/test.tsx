ffc61a22b3ee8a66a459dbe1a88701abff4bc29c
seveshy <danielalvesmajor@outlook.com>
2020-07-31 09:52:48 -0300

    Create styles first component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -11,11 +11,36 @@ describe('<Main />', () => {
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchInlineSnapshot(`
-      <main>
-        <h1>
-          React avançado
+      <main
+        class="styles__Wrapper-sc-1cs6c8-0 bPzoTz"
+      >
+        <img
+          alt="Imagem de um átomo e React Avançado escrito ao lado."
+          class="styles__Logo-sc-1cs6c8-1 kjzhTH"
+          src="logo.svg"
+        />
+        <h1
+          class="styles__Title-sc-1cs6c8-2 gcXVYJ"
+        >
+          React Avançado
         </h1>
+        <h2
+          class="styles__Description-sc-1cs6c8-3 dmIau"
+        >
+          TypeScript, ReactJS, NextJS, e Styled Components
+        </h2>
+        <img
+          alt="Um desenvolvedor de frente para uma tela com códgo"
+          class="styles__Illustration-sc-1cs6c8-4 htelnY"
+          src="hero-illustration.svg"
+        />
       </main>
     `)
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
